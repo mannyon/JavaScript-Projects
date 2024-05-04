42f9bcab83323f8a6ad8a19d56b0792acec852eb
Manish <mannyinwork@gmail.com>
2024-05-04 18:56:14 +0530

    sdfs

--- QRCodeGenerator/script.js
+++ QRCodeGenerator/script.js
@@ -4,7 +4,7 @@ let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
 function generateQr() {
-    if(qrText.value.length > 0){
+    if (qrText.value.length > 0) {
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
         imgBox.classList.add("show-img");
     }
@@ -12,11 +12,42 @@ function generateQr() {
         qrText.classList.add('error');
         let errorSound = document.getElementById("errorSound");
         errorSound.play();
-        setTimeout(()=>{
+        showToast(invalidMsg)
+        setTimeout(() => {
             qrText.classList.remove('error');
-        },1000);
+        }, 1000);
 
     }
+}
+
+
+// **************buttons***********
+
+
+let toastBox = document.getElementById('toastBox');
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error!';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
+
+
+function showToast(msg) {
+
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+
+
+    toastBox.appendChild(toast);
+
+
+    if (msg.includes('Invalid')) {
+        toast.classList.add('invalid')
+
+    }
+
+    setTimeout(() => {
+        toast.remove();
+    }, 6000)
 }
 
 
