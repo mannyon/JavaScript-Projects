5b24dd563e95a786c7a159b8a66f6f30d10b4d3f
Manish <mannyinwork@gmail.com>
2024-05-09 09:41:35 +0530

    sdfd

--- StopWatch/script.js
+++ StopWatch/script.js
@@ -1,13 +1,13 @@
-let [seconds, minutes, hours] = [0,0,0];
+let [seconds, minutes, hours] = [0, 0, 0];
 let displayTime = document.getElementById("displayTime");
 let timer = null;
 
 function stopwatch() {
     seconds++;
-    if(seconds == 60){
+    if (seconds == 60) {
         seconds = 0;
         minutes++;
-        if(minutes == 60){
+        if (minutes == 60) {
             minutes = 0;
             hours++;
         }
@@ -18,9 +18,9 @@ function stopwatch() {
     let s = seconds < 10 ? "0" + seconds : seconds;
     displayTime.innerHTML = h + ":" + m + ":" + s;
 }
-
+//done
 function watchStart() {
-    if(timer!== null){
+    if (timer !== null) {
         clearInterval(timer);
     }
     timer = setInterval(stopwatch, 1000);
@@ -32,6 +32,6 @@ function watchStop() {
 
 function watchReset() {
     clearInterval(timer);
-    [seconds, minutes, hours] = [0,0,0];
+    [seconds, minutes, hours] = [0, 0, 0];
     displayTime.innerHTML = "00:00:00";
 }
