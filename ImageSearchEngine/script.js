8b2c4d5f42bded21321221c365646bfad41df13e
Manish <mannyinwork@gmail.com>
2024-05-29 14:48:59 +0530

    dsf

---
+++ ImageSearchEngine/script.js
@@ -0,0 +1,50 @@
+const accessKey = "tKC9-sEtM3Y-KpvC8vxpmcSmTuuclskD4F-siBU0K28";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+// access key - tKC9-sEtM3Y-KpvC8vxpmcSmTuuclskD4F-siBU0K28
+// secret key - 6ouF_rNlGbWu973TMBiCkoiD5YWpr5x3iiv0w3SQW4o
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if(page === 1){
+        searchResult.innerHTML = "";
+    }
+
+    const results = data.results;
+    // console.log(data);
+
+    results.map((result) =>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+    showMoreBtn.style.display = "block";
+}
+
+searchForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMoreBtn.addEventListener("click", () =>{
+    page++;
+    searchImages();
+})
